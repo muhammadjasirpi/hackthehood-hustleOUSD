8c0d2a685eea270ea3381c8f3ef9015333ee4ca9
Jasir Muhammad <juma7muhammad@gmail.com>
2025-06-23 12:31:05 -0700

    update to data_structures.py

--- data_structures.py
+++ data_structures.py
@@ -2,3 +2,9 @@ hobbies = ["reading", "hiking", "coding", "gaming"]
 
 hobbies[0] = "coding"
 print(hobbies[0:4])
+
+idd = ("41252", '39123', '93812')
+numbs = [1,2,5,6,7] * 15
+print("#" .join(idd))
+print(numbs[0] + numbs[3] + numbs[4])
+print(f"Leon where are you? {idd}")
