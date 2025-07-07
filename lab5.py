45ee663919ab585b1724685d2f87df15d0992a7f
Jasir Muhammad <s_jasir.muhammad@ousd.org>
2025-07-07 12:53:15 -0700

    update to lab5.py

--- lab5.py
+++ lab5.py
@@ -1,22 +1,23 @@
 def cat_greeting():
-    print ("Cat says meow")
-cat_greeting
+    print("Cat says meow")
+cat_greeting()
 
-def genereate_superhero_power():
-    ##print("Flying")
+def generate_superhero_power():
     user_name = input("Enter your superhero name: ")
     super_power = input("Enter your super power: ")
-    print(f"{user_name} has the power of {super_power}!")
-genereate_superhero_power()
+    message = user_name + " has the power of " + super_power + "!"
+    return message
+generate_superhero_power()
 
 def cat_greeting_loop():
-    for i in range(5):
-        print("Cat says meow")
+    greetings = ["meow", "purr", "hiss", "mew"]
+    for i in greetings:
+        print("Cat says", i)
 cat_greeting_loop()
 
 def generate_multiple_powers():
-    powers = ["Flying, Super Strength, Invisibility"]
+    powers = ["Flying", "Super Strength", "Invisibility"]
     for power in powers:
-        print ("".join(powers))
+        print(power)
 generate_multiple_powers()
 
